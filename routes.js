3e835066cc1cdf5577ff28c1f79742a75ee4cfc8
paul.larripa <plarrip@gmail.com>
2025-06-07 13:17:15 +0200

    anyway..

---
+++ routes.js
@@ -0,0 +1,95 @@
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+
+
+module.exports = function (app, myDataBase){
+
+    //middleware to ensure connection to MongoDB before rendering '/profile' page
+    function ensureAuthenticated(req, res, next) {
+        if (req.isAuthenticated()) {
+        return next();
+        }
+        res.redirect('/');
+    };
+
+    app.route('/').get((req, res) => {
+        // Change the response to render the Pug template (login page)
+        res.render('index', {
+        title: 'Connected to Database',
+        message: 'Please login',
+        showLogin: true,
+        showRegistration: true,
+        showSocialAuth: true
+        });
+    });
+
+    // LOGIN ROUTE - POST request with passport authentication ('local' strategy)
+    app.route('/login')
+        .post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
+        res.redirect('/profile');
+        });
+
+    // PROFILE ROUTE - Render profile page
+    app.route('/profile')
+        .get(ensureAuthenticated, (req, res) => {
+        res.render('profile', { username: req.user.username });
+        });
+
+    
+    // LOGOUT route
+    app.route('/logout')
+        .get((req, res) => {
+        req.logout();
+        res.redirect('/');
+    });
+
+    app.route('/register')
+    .post((req, res, next) => {
+        const hash = bcrypt.hashSync(req.body.password, 12);
+        myDataBase.findOne({ username: req.body.username }, (err, user) => {
+        if (err) {
+            next(err);
+        } else if (user) {
+            res.redirect('/');
+        } else {
+            myDataBase.insertOne({
+            username: req.body.username,
+            password: hash
+            },
+            (err, doc) => {
+                if (err) {
+                res.redirect('/');
+                } else {
+                // The inserted document is held within
+                // the ops property of the doc
+                next(null, doc.ops[0]);
+                }
+            }
+            )
+        }
+        })
+    },
+        passport.authenticate('local', { failureRedirect: '/' }),
+        (req, res, next) => {
+        res.redirect('/profile');
+        }
+    );
+
+    app.route('/auth/github')
+        .get(passport.authenticate('github'));
+
+    app.route('/auth/github/callback')
+        .get(passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
+            res.redirect('/profile');
+        }
+    );
+
+
+    // Handling missing pages (Error 404)
+    app.use((req, res, next) => {
+        res.status(404)
+        .type('text')
+        .send('Not Found');
+    });
+
+}
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 'use strict';
 require('dotenv').config();
+console.log('Environment variables loaded in auth.js');
+console.log('All env vars:', Object.keys(process.env).filter(key => key.includes('GITHUB')));
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const passport = require('passport');
+const session = require('express-session');
+const routes = require('./routes.js');
+const auth = require('./auth.js');
 
 const app = express();
 
@@ -11,10 +17,37 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route('/').get((req, res) => {
 
+//session handling (express) + rendering page templates (pug)
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+app.use(passport.initialize(), passport.session());
+
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
+
+
+
+//handle MongoDB connection
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
+
+  auth(app, myDataBase);
+  routes(app, myDataBase);
+
+
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render('index', { title: e, message: 'Unable to connect to database' });
+  });
 });
 
+
+// run the code above
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
